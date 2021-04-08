0e82e974aa501793cb1e63e46910ecaf99d1cda0
AbonyiXavier <francisabonyi@gmail.com>
2021-04-08 16:29:01 +0100

    refactor start script

--- server/server.js
+++ server/server.js
@@ -1,13 +1,7 @@
-import http from 'http';
 import app from './app';
-const { config } = require('dotenv');
 
-config();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.NODE_ENV === 'test' ? 6378 : process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
 });
